a725968b437d6a7164a995b488440a3055830c3b
Your Name <you@example.com>
2025-07-11 15:31:50 +0800

    second

--- pythonGem1.py
+++ pythonGem1.py
@@ -3,3 +3,21 @@ import google.generativeai as genai
 import os 
 from dotenv import load_dotenv 
 
+load_dotenv()
+
+
+api_key = os.getenv('GOOGLE_API_KEY')
+# print(f"Value of GOOGLE_API_KEY from environment: {api_key}")
+if not api_key:
+    raise ValueError("Google api key not set")
+
+genai.configure(api_key=api_key)
+
+print("Gemini API configured!")
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+user_prompt = "what is generative ai "
+response = model.generate_content(user_prompt)
+print("\n-- Gemini's Response: ")
+print(response.text)
